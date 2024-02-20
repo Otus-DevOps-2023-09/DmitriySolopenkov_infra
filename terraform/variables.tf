ce9fd414adffcc80f163a0d34992f072f21932f9
Dmitriy Solopenkov <dsolopenkov@astralinux.ru>
2024-02-20 11:58:45 +0300

    HW 8. Terraform-1

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "yandex_cloud_id" {}
+
+variable "yandex_folder_id" {}
+
+variable "yandex_service_account_key_file" {}
+
+variable "yandex_image_id" {}
+
+variable "yandex_subnet_id" {}
+
+variable "public_key_path" {}
+
+variable "private_key_path" {}
